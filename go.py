8f673dfd49967d922528dc75ff2001e744c7518b
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-24 14:05:58 +0700

    Add files via upload

--- go.py
+++ go.py
@@ -9,12 +9,12 @@ import os
 def display_menu():
     print("Menu:")
     print("1. Cari Sub Domain (Koneksi Internet) ")
-    print("2. Convert Hasil")
-    print("3. Scan WS Direct (Koneksi paket data)")
-    print("4. Scan WS PORT 80 443 ")
-    print("5. Scan SNI  ")
-    print("6. Ping SNI  ")
-    print("7. Sumber PCAPdroid (isi sumber_pcapdroid.txt) ")
+    print("2. Pilih file (Koneksi data Bug) ")
+    
+    print("3. Scan WS PORT 80 443 ")
+    print("4. Scan SNI  ")
+    print("5. Ping SNI  ")
+    print("6. Sumber PCAPdroid (isi sumber_pcapdroid.txt) ")
        
     print("0. Keluar")
 
@@ -23,17 +23,16 @@ def run_file(file_number):
         os.system("python3 k_subdomain1.py")
     elif file_number == 2:
         os.system("python3 k_subdomain2.py")
+    
     elif file_number == 3:
-        os.system("python3 ws.py")
-    elif file_number == 4:
         os.system("python3 ws2.py")
         
-    elif file_number == 5:
+    elif file_number == 4:
         os.system("python3 sni1.py")
-    elif file_number == 6:
+    elif file_number == 5:
         os.system("python3 sni2.py")
         
-    elif file_number == 7:
+    elif file_number == 6:
         os.system("python3 datalama.py")    
          
         
--- k_subdomain1.py
+++ k_subdomain1.py
@@ -3,6 +3,6 @@ os.system("clear")
 print("\n GUNAKAN KONEKSI INTERNET  \n\n")
 domain = input("Masukan Domain: ")
 
-os.system(f"python3 knockpy.py  {domain} --no-local --no-http-code 404 500 530 -o hasil")
+os.system(f"python3 knockpy.py  {domain} --no-http-code 404 500 530 -th 10 -o hasil")
 print("\n\n  Data disimpan di folder hasil \n\n ")
 
